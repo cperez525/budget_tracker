5bcc230a10010768ae7005a207bdc4dfcfd52f52
Cris Perez <cristino.perez525@gmail.com>
2020-11-12 15:58:49 -0600

    Attempting to resolve error on deployed app

--- public/service-worker.js
+++ public/service-worker.js
@@ -7,7 +7,7 @@ const FILES_TO_CACHE = [
     'styles.css',
     '/icons/icon-192x192.png',
     '/icons/icon-512x512.png',
-    '../models/transaction.js'
+    '../../models/transaction.js'
 ];
 
 const PRECACHE = 'precache-v1';
